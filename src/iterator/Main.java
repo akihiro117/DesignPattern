090f575777586e6a2d229a53ebdd52373060557a
Akihiro Yamada <u.parr917@gmail.com>
2020-02-20 00:53:23 +0900

    配列版 の iterator を使用。

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -3,18 +3,33 @@ package iterator;
 public class Main {
 
     public static void main(String[] args) {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName("The Counter");
-        restaurant.setGenre("ハンバーガー");
-        restaurant.setLocation("六本木");
+        Restaurant humbergerRestaurant = new Restaurant();
+        humbergerRestaurant.setName("The Counter");
+        humbergerRestaurant.setGenre("ハンバーガー");
+        humbergerRestaurant.setLocation("六本木");
 
         RestaurantList restaurantList = new RestaurantList();
-        restaurantList.addRestaurant(restaurant);
+        restaurantList.addRestaurant(humbergerRestaurant);
 
-        Iterator restaurantIterator = restaurantList.createIterator();
+        Iterator restaurantListIterator = restaurantList.createIterator();
+        printRestaurant(restaurantListIterator);
+
+        Restaurant curryRestaurant = new Restaurant();
+        curryRestaurant.setName("オードリー");
+        curryRestaurant.setGenre("カレー");
+        curryRestaurant.setLocation("神保町");
+
+        RestaurantArray restaurantArray = new RestaurantArray(0);
+        restaurantArray.addRestaurant(curryRestaurant);
+
+        Iterator restaurantArrayIterator = restaurantArray.createIterator();
+        printRestaurant(restaurantArrayIterator);
+
+    }
+
+    public static void printRestaurant(Iterator restaurantIterator) {
         while (restaurantIterator.hasNext()) {
             System.out.println(restaurantIterator.next().toString());
         }
     }
-
 }
