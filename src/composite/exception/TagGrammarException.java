56920cad1b0417f987cd035335740aa511f42f04
Akihiro Yamada <u.parr917@gmail.com>
2021-06-05 18:08:46 +0900

    compositeパターンを追加

---
+++ src/composite/exception/TagGrammarException.java
@@ -0,0 +1,10 @@
+package composite.exception;
+
+public class TagGrammarException extends RuntimeException {
+    public TagGrammarException() {
+    }
+
+    public TagGrammarException(String msg) {
+        super(msg);
+    }
+}
